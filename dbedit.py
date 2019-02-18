457c788ab084cde0b059b400538eb0cbf57731b2
Daniel <db_win10_login@outlook.com>
2019-02-18 01:23:26 +0000

    added searchbar

--- dbedit.py
+++ dbedit.py
@@ -4,10 +4,8 @@ import tkinter as tk
 class App(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self,master)
-
         self.db=sql.connect("itemdb.s3db")
-        
-        self.grid(sticky=tk.N+tk.S)
+        self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
         self.createWidgets()
 
 
@@ -48,7 +46,7 @@ class App(tk.Frame):
         self.itemReq_ReqIDLabel = tk.Label(self,text="Req ID:")
         self.itemReq_ReqIDLabel.grid(row=ri,column=0,columnspan=2,sticky=tk.W+tk.N)
         self.itemReq_ReqIDEntry = tk.Entry(self, textvariable=self.itemReq_ReqIDSVar)
-        self.itemReq_ReqIDEntry.grid(row=ri,column=2,columnspan=2,sticky=tk.N+tk.E+tk.W)
+        self.itemReq_ReqIDEntry.grid(row=ri,column=2,columnspan=2,sticky=tk.W+tk.E+tk.N)
 
 
     def createItemReqAmtRow(self,ri):
@@ -56,7 +54,7 @@ class App(tk.Frame):
         self.itemReq_AmtLabel = tk.Label(self,text="Amount:")
         self.itemReq_AmtLabel.grid(row=ri,column=0,columnspan=2,sticky=tk.W+tk.N)
         self.itemReq_AmtEntry = tk.Entry(self, textvariable=self.itemReq_AmtSVar)
-        self.itemReq_AmtEntry.grid(row=ri,column=2,columnspan=2,sticky=tk.N+tk.E+tk.W)
+        self.itemReq_AmtEntry.grid(row=ri,column=2,columnspan=2,sticky=tk.W+tk.E+tk.N)
 
 
     def createItemReqButtonsRow(self,ri):
@@ -106,6 +104,7 @@ class App(tk.Frame):
         self.item_ItemListbox = tk.Listbox(self,listvariable=self.item_ItemListboxSVar,yscrollcommand=self.item_ItemListboxYScroll.set)
         self.item_ItemListbox.grid(row=top,rowspan=ri,column=4,sticky=tk.N+tk.S+tk.E+tk.W)
         self.item_ItemListboxYScroll['command'] = self.item_ItemListbox.yview
+        self.grid_columnconfigure(4,weight=1)
 
 
     def createItemReqListboxColumn(self,top,ri):
@@ -114,8 +113,25 @@ class App(tk.Frame):
         self.itemReq_ReqListboxYScroll.grid(row=top,rowspan=ri,column=7,sticky=tk.E+tk.W+tk.N+tk.S)
         self.itemReq_ReqListbox = tk.Listbox(self,listvariable=self.itemReq_ReqListboxSVar,yscrollcommand=self.itemReq_ReqListboxYScroll.set)
         self.itemReq_ReqListbox.grid(row=top,rowspan=ri,column=6,sticky=tk.N+tk.S+tk.E+tk.W)
-        self.itemReq_ReqListboxYScroll['command'] = self.itemReq_ReqListbox.yview        
+        self.itemReq_ReqListboxYScroll['command'] = self.itemReq_ReqListbox.yview
+        self.grid_columnconfigure(6,weight=3)
         
+
+    def createCalcLabRow(self,ri):
+        self.calc_LabAmtSVar = tk.StringVar()
+        self.calc_LabAmtLabel = tk.Label(self,text="Number of labs: ")
+        self.calc_LabAmtLabel.grid(row=ri,column=0,columnspan=3,sticky=tk.W+tk.N)
+        self.calc_LabAmtEntry = tk.Entry(self,textvariable=self.calc_LabAmtSVar)
+        self.calc_LabAmtEntry.grid(row=ri,column=3,columnspan=1,sticky=tk.W+tk.E+tk.N)
+
+
+    def createCalcTimeRow(self,ri):
+        self.calc_ResearchTimeSVar = tk.StringVar()
+        self.calc_ResearchTimeLabel = tk.Label(self,text="Research time per sci unit:")
+        self.calc_ResearchTimeLabel.grid(row=ri,column=0,columnspan=3,sticky=tk.W+tk.N)
+        self.calc_ResearchTimeEntry = tk.Entry(self,textvariable=self.calc_ResearchTimeSVar)
+        self.calc_ResearchTimeEntry.grid(row=ri,column=3,columnspan=1,sticky=tk.W+tk.E+tk.N)     
+
 
     def createWidgets(self):
         self.master.title('Factorio Item Database')
@@ -126,9 +142,14 @@ class App(tk.Frame):
         # the root window
         top=self.winfo_toplevel()
         
+        #top['width']=800
+        #top['height']=600
+        #top.grid_propagate(0)
+        
         # rowconfigure tells the window that whatever is in row 0 (arg0)
         # should resize at a rate of 1 (arg1,'weight')
         # weight is a ratio compared to other widgets
+        # widget will resize by (self.weight/total.weight) percent of total width
         # columnconfigure is rowconfigure except for columns
         
         top.rowconfigure(0,weight=1)
@@ -155,41 +176,42 @@ class App(tk.Frame):
         ri+=1
         self.createSciCheckboxBottomRow(ri)
         ri+=1
-
-
-        # calculation widgets
-        self.calc_LabAmtSVar = tk.StringVar()
-        self.calc_LabAmtLabel = tk.Label(self,text="Number of labs: ")
-        self.calc_LabAmtLabel.grid(row=ri,column=0,columnspan=3)
-        self.calc_LabAmtEntry = tk.Entry(self,textvariable=self.calc_LabAmtSVar)
-        self.calc_LabAmtEntry.grid(row=ri,column=3,columnspan=1)
+        self.createCalcLabRow(ri)
         ri+=1
-
-        self.calc_ResearchTimeSVar = tk.StringVar()
-        self.calc_ResearchTimeLabel = tk.Label(self,text="Research time per sci unit:")
-        self.calc_ResearchTimeLabel.grid(row=ri,column=0,columnspan=3)
-        self.calc_ResearchTimeEntry = tk.Entry(self,textvariable=self.calc_ResearchTimeSVar)
-        self.calc_ResearchTimeEntry.grid(row=ri,column=3,columnspan=1)
+        self.createCalcTimeRow(ri)
         ri+=1
-
-
+        
         # separator, top-anchored
 
-        # this tells the grid to resize this row, it will expand with the window
+        # this tells the grid to resize the last row (calctimerow), it will expand with the window
         # which expands with the root window... if the root window doesn't resize
         # nothing else will!
-        self.grid_rowconfigure(ri-1,weight=1)        
+        self.grid_rowconfigure(ri-1,weight=1)
 
 
         self.createItemListboxColumn(0,ri)
         self.createItemReqListboxColumn(0,ri)
         ri+=1
+
+        self.itemSearchSVar = tk.StringVar()
+        self.itemSearchLabel = tk.Label(self,text="Searchbar:")
+        self.itemSearchLabel.grid(row=ri,column=3,sticky=tk.E)
+        self.itemSearchEntry = tk.Entry(self,textvariable=self.itemSearchSVar)
+        self.itemSearchEntry.grid(row=ri,column=4,sticky=tk.E+tk.W+tk.N+tk.S)
+        self.itemSearchEntry.bind('<Any-KeyRelease>',self.updateItemList)
+
+        self.itemReqSearchSVar = tk.StringVar()
+        self.itemReqSearchEntry = tk.Entry(self,textvariable=self.itemReqSearchSVar)
+        self.itemReqSearchEntry.grid(row=ri,column=6,sticky=tk.E+tk.W+tk.N+tk.S)
+        self.itemReqSearchEntry.bind('<Any-KeyRelease>',self.updateItemReqList)
         
         
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
         self.quitButton.grid(row=ri,column=6,sticky=tk.N+tk.E)
 
-        self.updateList()
+
+        self.updateItemList()
+        self.updateItemReqList()
 
 
     def addItem(self):
@@ -198,34 +220,33 @@ class App(tk.Frame):
             t=self.item_TimeSVar.get()
             t=float(t)
             c=self.db.cursor()
-
-            
-            c.execute('SELECT id FROM item WHERE name LIKE ? ;',(n,))
-            if len(c.fetchall()) > 0:
-                print("addItem(): Item name already in database.")
-                return
             
             c.execute('INSERT INTO item(name,time) VALUES(?,?);',(n,t))
+            c.close()
             self.db.commit()
-            self.updateList()
+            self.item_NameSVar.set("")
+            self.item_TimeSVar.set("")
+            self.updateItemList()
                  
         except Exception as e:
-            print( 'Exception occured while adding item:\n' + str(e) )
+            print( 'delItem(): Exception occured while adding item:\n' + str(e) )
 
 
     def delItem(self):
         try:
-            tgt = self.itemListbox.get(self.itemListbox.curselection())
-            tid = tgt.split(":")[0]
-            c=self.db.cursor()
-
-            
-            c.execute("DELETE FROM item WHERE id=?;",(tid,))
+            deltuple=self.item_ItemListbox.curselection()
+            for d in deltuple:
+                tgt = self.item_ItemListbox.get()
+                tid = tgt.split(":")[0]
+                c=self.db.cursor()
+                c.execute("DELETE FROM item WHERE id=?;",(tid,))
+                
+            c.close()
             self.db.commit()
-            self.updateList()
+            self.updateItemList()
             
         except Exception as e:
-            print('Exception occured while adding item:\n' + str(e))
+            print('delItem(): Exception occured while deleting item:\n' + str(e))
 
 
     def updateItem(self):
@@ -241,8 +262,9 @@ class App(tk.Frame):
 
 
             c.execute( "INSERT INTO itemReqs(itemID,reqID,amt) VALUES(?,?,?);", (iid,rid,amt)  )
+            c.close()
             self.db.commit()
-            self.updateList()
+            self.updateItemReqList()
 
             
         except Exception as e:
@@ -258,40 +280,52 @@ class App(tk.Frame):
 
 
             c.execute("DELETE FROM itemReqs WHERE itemID=? AND reqID=? AND amt=?;", (iid,rid,amt) )
+            c.close()
             self.db.commit()
-            self.updateList()
+            self.updateItemReqList()
 
             
         except Exception as e:
             print("delReq(): Exception occured while deleting requirement:\n" + str(e))
 
 
-    def updateList(self):
+    def updateItemList(self,event=None):
         try:
             c=self.db.cursor()
-            
+            searchTerm=self.itemSearchSVar.get()
             s=""
-            c.execute('SELECT id,name,time FROM item;')
+            c.execute( 'SELECT id,name,time FROM item WHERE name LIKE ? ;', ("%"+searchTerm+"%",) )
             for i in c.fetchall():
                 s+=str(i[0])+":"+str(i[1].replace(" ","\ "))+":"+str(i[2])+" "
 
             self.item_ItemListboxSVar.set(s)
+            c.close()
+            
+        except Exception as e:
+            print("updateItemList(): Exception occured while updating lists:\n" + str(e))
 
-
+            
+    def updateItemReqList(self,event=None):
+        try:
+            c=self.db.cursor()
+            searchTerm=self.itemReqSearchSVar.get()
             s=""
-            c.execute("SELECT i.name,i.id,r.name,r.id,ir.amt FROM item AS r \
+            c.execute('SELECT i.name,i.id,r.name,r.id,ir.amt FROM item AS r \
                       JOIN itemReqs AS ir ON r.id = ir.reqID \
-                      JOIN item AS i ON i.id = ir.itemID;")
+                      JOIN item AS i ON i.id = ir.itemID \
+                      WHERE r.name LIKE ? or i.name LIKE ? ;',
+                      ( ("%"+str(searchTerm)+"%","%"+str(searchTerm)+"%") )
+                      )
             for i in c.fetchall():
                 for j in i:
                     s+=str(j).replace(" ","\ ")+":"
                 s=s[:-1]+" "
 
             self.itemReq_ReqListboxSVar.set(s)
+            c.close()
 
-            
         except Exception as e:
-            print("updateList(): Exception occured while updating lists:\n" + str(e))
+            print("updateItemReqList(): Exception occured while updating lists:\n" + str(e))
             
         
     def quit(self):
